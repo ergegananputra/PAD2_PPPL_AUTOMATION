3f2b327c6b182ddb00572709b6606ec358ec2ad9
MayCleanSitepu <113108027+MayCleanSitepu@users.noreply.github.com>
2024-06-25 19:45:13 +0700

    edit salah page

--- src/test/java/stepDefinitions/PublicKajianMainStepDef.java
+++ src/test/java/stepDefinitions/PublicKajianMainStepDef.java
@@ -7,20 +7,21 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
+import pages.kajian.main.kajian.KajianPage;
 
 import static org.junit.Assert.assertTrue;
 
 public class PublicKajianMainStepDef {
 
     static WebDriver driver;
-//    static PublicKajianMainPage publicKajianMainPage;
+    static KajianPage KajianMainPage;
 
     @BeforeAll
     void setup() {
         try {
             driver = Hooks.getDriver();
-            if (publicKajianMainPage == null) {
-                publicKajianMainPage = new PublicKajianMainPage(driver);
+            if (KajianMainPage == null) {
+                KajianMainPage = new KajianPage(driver);
             }
 
             Hooks.test.log(Status.INFO, "Setup Driver on PublicKajianMainStepDef");
@@ -33,7 +34,7 @@ public class PublicKajianMainStepDef {
     public void a_web_browser_is_at_the_mwcc_main_page() {
         try {
             setup();
-            String actualURL = publicKajianMainPage.getURL();
+            String actualURL = KajianMainPage.getURL();
             String containURL = "https://dev.mwcc.masmoendigital.store/";
 
             assertTrue(actualURL.contains(containURL));
@@ -47,7 +48,7 @@ public class PublicKajianMainStepDef {
     @When("the users clicks the Kajian on the navigation bar")
     public void theUsersClicksTheKajianOnTheNavigationBar() {
         try {
-            publicKajianMainPage.clickMenuKajian();
+            KajianMainPage.clickMenuKajian();
             Hooks.test.log(Status.INFO, "Click Kajian on the navigation bar");
         } catch (Exception e) {
             Hooks.test.log(Status.FAIL, "Failed to click Kajian on the navigation bar: " + e.getMessage());
@@ -57,7 +58,7 @@ public class PublicKajianMainStepDef {
     @Then("list of kajian are shown on the result page")
     public void listOfKajianAreShownOnTheResultPage() {
         try {
-            assert publicKajianMainPage.isOnPage();
+            assert KajianMainPage.isOnPage();
             Hooks.test.log(Status.PASS, "List of kajian are shown on the result page");
         } catch (AssertionError e) {
             Hooks.test.log(Status.FAIL, "Failed to retrieve list of kajian on the result page: " + e.getMessage());
